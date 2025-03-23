52137485e934dd13662e3e3e66b3db84f2df1495
karripartanen <karripartanen25@gmail.com>
2025-03-23 12:52:23 +0200

    some more models, no comments ofc

--- auth-server/src/api/models/userModel.ts
+++ auth-server/src/api/models/userModel.ts
@@ -87,4 +87,149 @@ const getUserByUsername = async (
   return rows[0];
 };
 
-export {getUserById, getUserByEmail, createUser, getUserByUsername};
+const deleteUser = async (
+  user_id: number,
+  token: string,
+): Promise<UserDeleteResponse> => {
+  const connection = await promisePool.getConnection();
+
+  try {
+    await connection.beginTransaction();
+
+    // join user_id from RecipePosts
+    const [recipeFiles] = await connection.execute<RowDataPacket[]>(
+      `SELECT filename FROM RecipeMedia
+      JOIN RecipePosts ON RecipeMedia.media_id = RecipePosts.media_id
+      WHERE RecipePosts.user_id = ?`,
+      [user_id],
+    );
+
+    if (recipeFiles.length > 0) {
+      recipeFiles.forEach(async (file) => {
+        const filename = file.filename.replace(
+          process.env.UPLOAD_URL as string,
+          '',
+        );
+        console.log('filename', filename);
+        const options = {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        };
+
+        try {
+          const response = await fetchData<MessageResponse>(
+            `${process.env.UPLOAD_SERVER}/delete/${filename}`,
+            options,
+          );
+
+          console.log('response', response);
+        } catch (error) {
+          console.error((error as Error).message);
+        }
+      });
+    }
+
+    const options = {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({user_id}),
+    };
+
+    const existingProfilePic = await checkProfilePicExists(user_id);
+
+    if (existingProfilePic?.file_path) {
+      try {
+        const response = await fetchData<MessageResponse>(
+          `${process.env.UPLOAD_SERVER}/profilepicture/delete`,
+          options,
+        );
+        console.log('response', response);
+      } catch (error) {
+        console.error((error as Error).message);
+      }
+    }
+
+    // delete user content
+    await connection.execute('DELETE FROM Comments WHERE user_id = ?', [
+      user_id,
+    ]);
+    await connection.execute('DELETE FROM RecipePosts WHERE user_id = ?', [
+      user_id,
+    ]);
+    await connection.execute('DELETE FROM ProfilePictures WHERE user_id = ?', [
+      user_id,
+    ]);
+    await connection.execute('DELETE FROM Likes WHERE user_id = ?', [user_id]);
+    await connection.execute('DELETE FROM Ratings WHERE user_id = ?', [
+      user_id,
+    ]);
+
+    await connection.execute(
+      'DELETE FROM MediaTags WHERE media_id IN (SELECT media_id FROM MediaItems WHERE user_id = ?);',
+      [user_id],
+    );
+
+    await connection.execute(
+      'DELETE FROM Comments WHERE media_id IN (SELECT media_id FROM MediaItems WHERE user_id = ?);',
+      [user_id],
+    );
+
+    await connection.execute(
+      'DELETE FROM Likes WHERE media_id IN (SELECT media_id FROM MediaItems WHERE user_id = ?);',
+      [user_id],
+    );
+
+    const [result] = await connection.execute<ResultSetHeader>(
+      'DELETE FROM Users WHERE user_id = ?',
+      [user_id],
+    );
+
+    await connection.commit();
+
+    if (result.affectedRows === 0) {
+      throw new CustomError('User not deleted', 500);
+    }
+
+    return {message: 'User deleted successfully', 'user': {user_id}};
+
+  } catch (error) {
+    console.error((error as Error).message);
+    throw new CustomError('Error deleting user', 500);
+  } finally {
+    connection.release();
+  }
+};
+
+
+const checkProfilePicExists = async (
+  user_id: number,
+): Promise<ProfilePicture | null> => {
+  const [rows] = await promisePool.execute<RowDataPacket[] & ProfilePicture[]>(
+    `
+    SELECT
+      pp.profile_picture_id,
+      pp.user_id,
+      pp.filename
+      CONCAT(v.base_url, pp.filename) AS filename
+    FROM ProfilePictures pp
+    CROSS JOIN (SELECT ? AS base_url) AS v
+    WHERE pp.user_id = ?
+    `,
+    [profilePicDir, user_id],
+  );
+
+  if (rows.length === 0) {
+    return null;
+  }
+
+  return rows[0];
+};
+
+
+export {getUserById, getUserByEmail, createUser, getUserByUsername, deleteUser, checkProfilePicExists};
