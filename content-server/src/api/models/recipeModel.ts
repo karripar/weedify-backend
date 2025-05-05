d13deb621f74a4add5ec9754fac27478897c6738
karripartanen <karripartanen25@gmail.com>
2025-05-05 22:49:39 +0300

    change

--- content-server/src/api/models/recipeModel.ts
+++ content-server/src/api/models/recipeModel.ts
@@ -516,16 +516,16 @@ const fetchRecipesFromFollowedUsers = async (
           ELSE CONCAT(v.base_url, rp.filename, '-animation.gif')
       END AS thumbnail,
       CASE
-          WHEN rp.media_type NOT LIKE '%image%'
-          THEN JSON_ARRAY(
-              CONCAT(v.base_url, rp.filename, '-thumb-1.png'),
-              CONCAT(v.base_url, rp.filename, '-thumb-2.png'),
-              CONCAT(v.base_url, rp.filename, '-thumb-3.png'),
-              CONCAT(v.base_url, rp.filename, '-thumb-4.png'),
-              CONCAT(v.base_url, rp.filename, '-thumb-5.png')
+        WHEN rp.media_type NOT LIKE '%image%'
+        THEN JSON_ARRAY(
+            CONCAT(v.base_url, SUBSTRING_INDEX(rp.filename, '.', 1), '-thumb-1.png'),
+            CONCAT(v.base_url, SUBSTRING_INDEX(rp.filename, '.', 1), '-thumb-2.png'),
+            CONCAT(v.base_url, SUBSTRING_INDEX(rp.filename, '.', 1), '-thumb-3.png'),
+            CONCAT(v.base_url, SUBSTRING_INDEX(rp.filename, '.', 1), '-thumb-4.png'),
+            CONCAT(v.base_url, SUBSTRING_INDEX(rp.filename, '.', 1), '-thumb-5.png')
           )
           ELSE NULL
-      END AS screenshots,
+        END AS screenshots,
       (
           SELECT JSON_ARRAYAGG(
               JSON_OBJECT(
